1dfdce826893e158b613108fbaae4e779fa16202
tjuant1 <juanpatrickpds@gmail.com>
2024-10-16 01:18:31 -0300

    added login test

--- tests/blaze_home.spec.js
+++ tests/blaze_home.spec.js
@@ -2,7 +2,8 @@ const {test, expect} = require('@playwright/test')
 
 test('Home Page', async ({page})=>{
 
-    await page.goto('https://front.serverest.dev/login');
+    await page.goto('https://demoblaze.com/');
+    await page.setViewportSize({ width: 1920, height: 1080 });
 
     const pageURL = page.url();
     const pageTitle = await page.title();
